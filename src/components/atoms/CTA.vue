5cb03504e1b5f6abf4e77de18a92912e6d1d0d0b
StuartCowley <voidstalker@live.co.uk>
2021-10-10 11:50:05 +0100

    Move CTA styles into a mixin

--- src/components/atoms/CTA.vue
+++ src/components/atoms/CTA.vue
@@ -19,20 +19,7 @@ export default {
 	@import "src/scss/toolkit";
 	$c: '.cta';
 	#{$c} {
-		display: inline-block;
-		color: $offWhite;
-		background-color: $mainGold;
-		letter-spacing: 0.5rem;
-		padding-top: 8px;
-		padding-bottom: 8px;
-		padding-left: 24px;
-		padding-right: 24px;
-		border-radius: 40px;
-		text-transform: uppercase;
-		font-size: 1.6rem;
-		@media screen and (max-width: $breakpoint-mob) {
-			font-size: 1rem;
-		}
+		@include cta;
 	}
 
 </style>
