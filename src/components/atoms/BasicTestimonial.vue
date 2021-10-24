bc96a7b4215c1ca4bae1dd34d4c0f0b32aa8d759
StuartCowley <voidstalker@live.co.uk>
2021-10-24 23:58:47 +0100

    Add basic testimonial page

---
+++ src/components/atoms/BasicTestimonial.vue
@@ -0,0 +1,70 @@
+<template>
+	<div class="basic-testimonial">
+		<div class="grid__item">
+			<div class="basic-testimonial-wrap">
+				<div class="basic-testimonial__content">
+					<slot name="quote"></slot>
+				</div>
+				<div class="basic-testimonial__attribution">
+					<slot name="attribution"></slot>
+				</div>
+			</div>
+		</div>
+		<div
+			class="basic-testimonial__image"
+			:class="{'basic-testimonial__image--fill': $mq == 'mob' || $mq == 'ptab' || $mq == 'ltab' }"
+			:style="{backgroundImage: `url(${image})` }">
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'BasicTestimonial',
+}
+</script>
+
+<style lang="scss">
+@import 'src/scss/toolkit';
+
+$c:'.basic-testimonial';
+#{$c} {
+	.page-container {
+		padding-left: 3.5rem;
+		padding-right: 3.5rem;
+	}
+	&-wrap {
+		text-align: center;
+	}
+	&__content {
+		color: orange;
+		margin-bottom: 1rem;
+		font-size: 1.4rem;
+		@media screen and (max-width: $breakpoint-mob) {
+			font-size: 0.9rem;
+		}
+		&:before,
+		&:after {
+			content: '"';
+			font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+			font-size: 100px;
+			position: absolute;
+			color: grey;
+			opacity: 0.5;
+		}
+		&:before {
+			margin-top: -32px;
+			margin-left: -32px;
+		}
+		&:after {
+			margin-top: -24px;
+		}
+	}
+	&__attribution {
+		color: yellow;
+		font-style: italic;
+	}
+
+}
+
+</style>
--- src/components/organisms/TestimonialPage.vue
+++ src/components/organisms/TestimonialPage.vue
@@ -2,34 +2,39 @@
 	<div class="testimonial-page">
 		<SecondaryLayout>
 			<template v-slot:title>
-				title here
+				<div class="testimonial-page__header">
+					Testimonials
+				</div>
 			</template>
 
 			<template v-slot:content>
-				<Testimonial :reverse="true" bgImage="bride-2.jpg">
-					<template v-slot:quote>
-						AMAZING!! This band were absolutely incredible. We picked the Bees Knees because of Lauren’s voice and their varied set list. They exceeded all expectations and kept our wedding dancing all night. All the guests said what an amazing band we had and they really helped make our wedding so wonderful. They happily learnt our first dance song and even played requests from guests. Thank you Bees Knees!!
-					</template>
-					<template v-slot:attribution>
-						- Philip S, 2019
-					</template>
-				</Testimonial>
+				<div class="page-container">
+					<div class="testimonial-page__body">
+						<BasicTestimonial>
+							<template v-slot:quote>
+								AMAZING!! This band were absolutely incredible. We picked the Bees Knees because of Lauren’s voice and their varied set list. They exceeded all expectations and kept our wedding dancing all night. All the guests said what an amazing band we had and they really helped make our wedding so wonderful. They happily learnt our first dance song and even played requests from guests. Thank you Bees Knees!!
+							</template>
+							<template v-slot:attribution>
+								- Philip S, 2019
+							</template>
+						</BasicTestimonial>
+					</div>
+				</div>
 			</template>
-
 		</SecondaryLayout>
 	</div>
 </template>
 
 <script>
+import BasicTestimonial from '../atoms/BasicTestimonial.vue'
 import SecondaryLayout from './SecondaryLayout'
-import Testimonial from '../atoms/Testimonial'
 
 export default {
 	name: 'TestimonialPage',
 	components: {
-		SecondaryLayout,
-		Testimonial
-	}
+		BasicTestimonial,
+		SecondaryLayout
+	},
 }
 </script>
 
@@ -40,5 +45,51 @@ $c: ".testimonial-page";
 #{$c}
 {
 	@include secondary-page-layout;
+
+	&__header {
+		@include secondary-page-heading;
+	}
+
+	&__body {
+		@include secondary-page-body;
+		position: relative;
+		padding-top: 20px;
+		padding-bottom: 20px;
+		padding-left: 20px;
+		padding-right: 20px;
+		margin-bottom: 20px;
+	}
+
+	&__copy {
+		@include copy;
+		text-align: center;
+	}
+
+	&__testimonial {
+		// TODO add transition effects on scroll
+		border: 8px solid $white;
+		background-color: $offWhite;
+		border-radius: 20px;
+		margin-left: 3rem;
+    margin-right: 3rem;
+    margin-top: 2rem;
+		margin-bottom: 2rem;
+
+		.quote {
+			width: 80%;
+			line-height: 1.75;
+			padding-top: 2rem;
+			padding-bottom: 2rem;
+			padding-left: 2rem;
+			padding-right: 2rem;
+		}
+
+		.attribution {
+			margin-right: 2rem;
+			padding-bottom: 2rem;
+			text-align: right;
+			font-style: italic;
+		}
+	}
 }
 </style>
